4ccb64adb48074633e158f711b9a38a3f824bf11
raoqiang881016 <97460580+raoqiang881016@users.noreply.github.com>
2022-06-14 21:30:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,1 @@
-export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="hk.wuyouss.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+ZXhwb3J0IGRlZmF1bHQgewogICAgYXN5bmMgZmV0Y2gocmVxdWVzdCwgZW52KSB7CiAgICAgIGxldCB1cmwgPSBuZXcgVVJMKHJlcXVlc3QudXJsKTsKICAgICAgaWYgKHVybC5wYXRobmFtZS5zdGFydHNXaXRoKCcvJykpIHsKICAgICAgICB1cmwuaG9zdG5hbWU9ImpwLnd1eW91c3MuY29tIjsKICAgICAgICBsZXQgbmV3X3JlcXVlc3Q9bmV3IFJlcXVlc3QodXJsLHJlcXVlc3QpOwogICAgICAgIHJldHVybiBmZXRjaChuZXdfcmVxdWVzdCk7CiAgICAgIH0KICAgICAgLy8gT3RoZXJ3aXNlLCBzZXJ2ZSB0aGUgc3RhdGljIGFzc2V0cy4KICAgICAgcmV0dXJuIGVudi5BU1NFVFMuZmV0Y2gocmVxdWVzdCk7CiAgICB9CiAgfTs=
